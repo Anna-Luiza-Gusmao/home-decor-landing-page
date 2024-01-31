fec79512e83bcf19682757390d3530fda5f5bbe1
Anna Luiza Gusmão <87679298+Anna-Luiza-Gusmao@users.noreply.github.com>
2024-01-31 13:31:46 -0300

    add: scroll appear images from home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,19 +4,20 @@ import HomeImg3 from "../../assets/home/img3.jpg"
 
 import { DescriptionContainer, HomeContainer, KnowMoreButton } from "./styles"
 import Content from "./homeContent.json"
+import { ScrollAppearImage } from "./components/ScrollAppearImage"
 
 export function HomePage() {
     return (
         <HomeContainer>
-            <img src={HomeImg1} alt="Casa Decorada 1" />
+            <img src={HomeImg1} alt="Casa Decorada 1"/>
             <DescriptionContainer>
                 <div>
                     <h2>{Content.homeContent.description[0].title}</h2>
                     <p>{Content.homeContent.description[0].description}</p>
                     <KnowMoreButton>Saiba mais</KnowMoreButton>
                 </div>
-                <img src={HomeImg2} alt="Casa Decorada 2" />
-                <img src={HomeImg3} alt="Casa Decorada 3" />
+                <ScrollAppearImage imageKey="1" imageUrl={HomeImg2} alt="Casa Decorada 2" porcentScreen="100%"/>
+                <ScrollAppearImage imageKey="2" imageUrl={HomeImg3} alt="Casa Decorada 3" porcentScreen="-100%"/>
                 <div>
                     <h2>{Content.homeContent.description[1].title}</h2>
                     <p>{Content.homeContent.description[1].description}</p>
